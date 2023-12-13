67df0838f6df1d6badc08fb9cdce782eb495e393
Julian <jdegut@hotmail.fr>
2023-12-13 23:40:16 +0100

    New feature : random vs ai

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from matplotlib import pyplot as plt
 
 from game.connect4.connect4 import Connect4
 from game.connect4.utils import pieces
-from mcts.mcts import MCTS
 from neuralnetwork.alphazero import AlphaZero
 from neuralnetwork.model import Model
 from notif.notificationbot import NotificationBot
@@ -17,6 +16,55 @@ load_dotenv()
 
 MODEL_FILENAME_TO_USE = "run/Connect4/model_latest.pth"
 bot = NotificationBot(os.getenv("USER_KEY"), os.getenv("API_TOKEN"))
+
+
+def ai_vs_random(game, model):
+    player = 1
+    state = game.get_init_state()
+
+    while True:
+        if player == 1:
+            valid_moves = game.get_legal_moves(state)
+            action = np.random.choice(np.where(valid_moves == 1)[0])
+        else:
+            encoded_state = game.get_encoded_state(state)
+            tensor_state = torch.tensor(encoded_state, device=model.device).unsqueeze(0)
+            policy, value = model(tensor_state)
+            policy = torch.softmax(policy, 1).squeeze(0).detach().cpu().numpy()
+
+            valid_moves = game.get_legal_moves(state)
+            policy *= valid_moves
+            policy /= np.sum(policy)
+
+            action = np.argmax(policy)
+
+        state = game.apply_action(state, player, action)
+
+        value, is_terminal = game.get_value_and_ended(state, action)
+        if is_terminal:
+            return value if player == 1 else -value
+
+        player = game.get_opponent(player)
+
+
+def launch_randoms(n=1000):
+    print("Launching", n, "random games....")
+    game = Connect4()
+    model = Model(game, 9, 128)
+    loaded_model = torch.load(MODEL_FILENAME_TO_USE, map_location=model.device)
+    model.load_state_dict(loaded_model["model_state_dict"])
+    wins = 0
+    losses = 0
+
+    for i in range(n):
+        value = ai_vs_random(game, model)
+        if value > 0:
+            wins += 1
+        elif value < 0:
+            losses += 1
+
+    print("Random agent wins : ", wins, "/", n)
+    print("Random agent losses : ", losses, "/", n)
 
 
 def play_against_ai():
@@ -105,7 +153,8 @@ if __name__ == '__main__':
     print("Welcome aboard, can I help you ?\n")
     print("Enter 1 to train the AI with settings set...........")
     print("Enter 2 to play against the AI......................")
-    print("Enter 3 to show losses plot (only after training°...")
+    print("Enter 3 to make AI play n times against random......")
+    print("Enter 4 to show losses plot (only after training)...")
     choice = -1
     while choice == -1:
         try:
@@ -117,4 +166,6 @@ if __name__ == '__main__':
     if choice == 2:
         play_against_ai()
     if choice == 3:
+        launch_randoms()
+    if choice == 4:
         show_loss_plot()
